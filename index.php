1bfa4009efccbd0591c002b7d5a15ee30e4eb2f9
Menno Zweistra <menno@atissoftware.com>
2023-04-18 15:56:09 +0200

    Improved scraper with retrieving all discounted products

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <h1>Omdat iemand op de kleintjes moet letten.</h1>
 <form action="/scrape.php" method="POST">
     <label>Paste a AH product category url</label><br/>
-    <input type="text" name="url" value="https://www.ah.nl/producten/bier-en-aperitieven" size="40">
-    <input type="submit" value="Show products">
+    <input type="text" name="url" value="https://www.ah.nl/producten" size="40">
+    <input type="submit" value="Show discounted products">
 </form>
 </body>
 </html>
--- scrape.php
+++ scrape.php
@@ -3,43 +3,64 @@
 require __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\DomCrawler\Crawler;
 
-$url = $_POST["url"];
-if (empty($url)) {
-    echo "Yawn. You sent an empty url.";
-    exit;
+if (php_sapi_name() === 'cli') {
+    // Called from commandline
+    $url = "main.html";
+} else {
+    $url = $_POST["url"];
+    if (empty($url)) {
+        echo "Yawn. You sent an empty url.";
+        exit;
+    }
 }
 
 $html = file_get_contents($url);
 $crawler = new Crawler($html);
-$articles = $crawler->filterXPath('//article');
-echo "<pre>";
-$articles->each(function (Crawler $article) {
-    $title = $article->evaluate('div/div/a/strong/span')->text();
-    try {
-        $price = $article->evaluate('div/a/div/div/div')->text();
-    } catch (Exception $e) {
-        $price = $article->evaluate('div/div[1]/div/div[2]/span')->text();
-    }
-    try {
-        $image = $article->evaluate('div/a/figure/div/img')->attr('src');;
-    } catch (Exception $e) {
-        $image = $article->evaluate('a/figure/div/img')->attr('src');;
-    }
-    try {
-        $size = $article->evaluate('div[1]/a/div[1]/div/span')->text();
-    } catch (Exception $e) {
-        $size = $article->evaluate('div/div[1]/div/span')->text();
-    }
-
-//     I was unable to find brand and promotion in the page, or in the subsequent product page
-//     I don't know which data you would like me to select
-//     $brand = ""; // X
-//     $promotion = ""; // X
-
-    echo $title . PHP_EOL;
-    echo $image . PHP_EOL;
-    echo $price . PHP_EOL;
-    echo $size . PHP_EOL;
-    echo PHP_EOL;
+$categoriesCrawler = $crawler->filterXPath('//main/div[1]/div/div/div/div/div/a/@href');
+$categories = [];
+$categoriesCrawler->each(function (Crawler $category) use (&$categories) {
+    $categories[] = 'https://www.ah.nl' . $category->text();
 });
+
+$articles = [];
+foreach($categories as $url) {
+    sleep(1); //sleep for 1 second so we don't generate too much traffic
+    $html = file_get_contents($url);
+    $crawler = new Crawler($html);
+    $articlesCrawler = $crawler->filterXPath('//article');
+    try {
+        $articlesCrawler->each(function (Crawler $article) use (&$articles) {
+            $title = $article->evaluate('div/div/a/strong/span')->text();
+            try {
+                $price = $article->evaluate('div/a/div/div/div')->text();
+            } catch (Exception $e) {
+                $price = $article->evaluate('div/div[1]/div/div[2]/span')->text();
+            }
+            try {
+                $image = $article->evaluate('div/a/figure/div/img')->attr('src');;
+            } catch (Exception $e) {
+                $image = $article->evaluate('a/figure/div/img')->attr('src');;
+            }
+            try {
+                $size = $article->evaluate('div[1]/a/div[1]/div/span')->text();
+            } catch (Exception $e) {
+                $size = $article->evaluate('div/div[1]/div/span')->text();
+            }
+//             echo $title . PHP_EOL;
+//             echo $price . PHP_EOL;
+//             echo PHP_EOL;
+            $articles[] = ['title' => $title, 'price' => $price];
+        });
+    }   catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), PHP_EOL;
+    }
+}
+
+echo "<pre>";
+foreach ($articles as $article) {
+    echo $article['title'] . PHP_EOL;
+    echo $article['price'] . PHP_EOL;
+    echo PHP_EOL;
+}
 echo "</pre>";
+var_dump($articles);
